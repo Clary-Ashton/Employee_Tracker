f7531051016a43997770c99254efdc2531846578
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-03-07 22:06:53 +0000

    updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -7,30 +7,28 @@ DROP TABLE IF EXISTS job;
 DROP TABLE IF EXISTS employee;
 
 CREATE TABLE department (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   dept_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE job (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL NOT NULL,
-  department_id INT
-  FOREIGN KEY (department_id)
-  REFERENCES department(id)
-  ON DELETE SET NULL
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  job_id INT,
-  manager_id INT
-  FOREIGN KEY (job_id)
-  REFERENCES role(id)
-  ON DELETE SET NULL,
-  FOREIGN KEY (manager_id)
-  REFERENCES employee(id)
-  ON DELETE SET NULL
+  job_id INT UNSIGNED NOT NULL,
+  INDEX job_ind (job_id),
+  CONSTRAINT fk_job FOREIGN KEY (job_id) REFERENCES job(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  
 );
