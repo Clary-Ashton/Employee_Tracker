f2e552881895db8f41d8059cecb29bf334a12e2c
Clary Mawangi <78886789+Clary-Ashton@users.noreply.github.com>
2022-03-07 18:42:08 +0000

    connecting to the database

--- server.js
+++ server.js
@@ -10,3 +10,14 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'company_db',
+  });
+  
+  connection.connect(err => {
+    if (err) throw err;
+    console.log("connected to the company_db database");
